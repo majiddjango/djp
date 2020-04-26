81bbe849bd96eec1deb39510e4e37d4423191849
majiddjango <majid.trader165@gmail.com>
2020-04-26 12:04:19 +0430

    changig urls

--- djp/urls.py
+++ djp/urls.py
@@ -23,7 +23,7 @@ urlpatterns = [
     path('accounts/',include('accounts.urls')),
     path('',include('core.urls')),
     path('', include('pwa.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 
 
